c775eecd4edf08f761606a8282215324ec900878
Tarun Reddy Kavadi <tarunreddy.kavadi.79@gmail.com>
2023-03-11 16:55:23 +0000

    c

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://fashion-backend-65852.web.app/api";
+const BASE_URL = "https://fashion-backend-65852.web.app";
 // const TOKEN =
 //   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //     .accessToken || "";
