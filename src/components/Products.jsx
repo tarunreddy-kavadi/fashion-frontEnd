825ec733d55df8b42c899f590da01d77ae0cd253
Tarun Reddy Kavadi <tarunreddy.kavadi.79@gmail.com>
2023-03-14 17:58:13 +0000

    c

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -20,8 +20,8 @@ const Products = ({ cat, filters, sort }) => {
       try {
         const res = await axios.get(
           cat
-            ? `http://localhost:5000/api/products?category=${cat}`
-            : "http://localhost:5000/api/products"
+            ? `http://fashionbackend.herokuapp.com/api/products?category=${cat}`
+            : "http://fashionbackend.herokuapp.com/api/products"
         );
         setProducts(res.data);
       } catch (err) {}
